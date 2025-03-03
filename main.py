7f43231341f0220d6394a5b3f25a4dac8a5c640e
MengXu95 <meng.xu.vuw@gmail.com>
2025-03-03 15:43:32 +0800

    update

--- main.py
+++ main.py
@@ -20,93 +20,91 @@ if __name__ == '__main__':
     seed = int(sys.argv[2])
     algo = str(sys.argv[3])
 
-    dataset_name = "mN2h_1_5b2"
-    for i in range(3):
-        print('----------MTGP_niching_rental_original_' + str(i) + '_----------')
-        seed = i
-        GPmain_niching_rental_original.main(dataset_name, seed)
 
-    for i in range(3):
-        print('----------CCGP_niching_rental_original_' + str(i) + '_----------')
-        seed = i
-        CCGPmain_niching_rental_original.main(dataset_name, seed)
-
-
-    dataset_name = "lN2h_1_5b2"
-    for i in range(3):
-        print('----------MTGP_niching_rental_original_' + str(i) + '_----------')
-        seed = i
-        GPmain_niching_rental_original.main(dataset_name, seed)
-
-    for i in range(3):
-        print('----------CCGP_niching_rental_original_' + str(i) + '_----------')
-        seed = i
-        CCGPmain_niching_rental_original.main(dataset_name, seed)
-
-    for i in range(3):
-        print('----------PSO_rental_' + str(i) + '_----------')
-        seed = i
+    if algo == 'CCGP_niching':
+        print('----------CCGP_niching----------')
+        CCGPmain_niching.main(dataset_name, seed)
+    elif algo == 'CCGP_niching_rental':
+        print('----------CCGP_niching_rental----------')
+        CCGPmain_niching_rental.main(dataset_name, seed)
+    elif algo == 'CCGP_niching_rental_2':
+        for i in range(2):
+            print('----------CCGP_niching_rental_'+str(i)+'_----------')
+            seed = i
+            CCGPmain_niching_rental.main(dataset_name, seed)
+    elif algo == 'MTGP_niching':
+        print('----------MTGP_niching----------')
+        GPmain_niching.main(dataset_name, seed)
+    elif algo == 'MTGP_niching_rental':
+        print('----------MTGP_niching_rental----------')
+        GPmain_niching_rental.main(dataset_name, seed)
+    elif algo == 'MTGP_niching_rental_2':
+        for i in range(3):
+            print('----------MTGP_niching_rental_'+str(i)+'_----------')
+            seed = i
+            GPmain_niching_rental.main(dataset_name, seed)
+    elif algo == 'MTGP_niching_rental_original_3':
+        for i in range(3):
+            print('----------MTGP_niching_rental_original_'+str(i)+'_----------')
+            seed = i
+            GPmain_niching_rental_original.main(dataset_name, seed)
+    elif algo == 'CCGP_niching_rental_original_3':
+        for i in range(3):
+            print('----------CCGP_niching_rental_original_'+str(i)+'_----------')
+            seed = i
+            CCGPmain_niching_rental_original.main(dataset_name, seed)
+    elif algo == 'PSO_rental':
+        print('----------PSO_rental----------')
         PSO_main_rental.main(dataset_name, seed)
+    elif algo == 'PSO_rental_3':
+        for i in range(3):
+            print('----------PSO_rental_'+str(i)+'_----------')
+            seed = i
+            PSO_main_rental.main(dataset_name, seed)
+    elif algo == 'DRL':
+        print('----------DRL----------')
+        DRL_main.main(dataset_name, seed)
+    elif algo == 'testRuleMTGP':
+        print('----------testRuleMTGP----------')
+        testRuleMTGP.main(dataset_name, seed)
+    elif algo == 'S2Demo':
+        print('----------S2Demo----------')
+        S2Demo.main(dataset_name, seed)
+    elif algo == 'testRuleCCGP':
+        print('----------testRuleCCGP----------')
+        testRuleCCGP.main(dataset_name, seed)
+    elif algo == 'testRuleDRL':
+        print('----------testRuleDRL----------')
+        testRuleDRL.main(dataset_name, seed)
+    elif algo == 'trainRulesSPolicy':
+        print('----------trainAndTestRulesSPolicy----------')
+        trainRulesSPolicy.main(dataset_name, seed)
 
 
-
-
-
-    # if algo == 'CCGP_niching':
-    #     print('----------CCGP_niching----------')
-    #     CCGPmain_niching.main(dataset_name, seed)
-    # elif algo == 'CCGP_niching_rental':
-    #     print('----------CCGP_niching_rental----------')
-    #     CCGPmain_niching_rental.main(dataset_name, seed)
-    # elif algo == 'CCGP_niching_rental_2':
-    #     for i in range(2):
-    #         print('----------CCGP_niching_rental_'+str(i)+'_----------')
-    #         seed = i
-    #         CCGPmain_niching_rental.main(dataset_name, seed)
-    # elif algo == 'MTGP_niching':
-    #     print('----------MTGP_niching----------')
-    #     GPmain_niching.main(dataset_name, seed)
-    # elif algo == 'MTGP_niching_rental':
-    #     print('----------MTGP_niching_rental----------')
-    #     GPmain_niching_rental.main(dataset_name, seed)
-    # elif algo == 'MTGP_niching_rental_2':
-    #     for i in range(3):
-    #         print('----------MTGP_niching_rental_'+str(i)+'_----------')
-    #         seed = i
-    #         GPmain_niching_rental.main(dataset_name, seed)
-    # elif algo == 'MTGP_niching_rental_original_3':
-    #     for i in range(3):
-    #         print('----------MTGP_niching_rental_original_'+str(i)+'_----------')
-    #         seed = i
-    #         GPmain_niching_rental_original.main(dataset_name, seed)
-    # elif algo == 'CCGP_niching_rental_original_3':
-    #     for i in range(3):
-    #         print('----------CCGP_niching_rental_original_'+str(i)+'_----------')
-    #         seed = i
-    #         CCGPmain_niching_rental_original.main(dataset_name, seed)
-    # elif algo == 'PSO_rental':
-    #     print('----------PSO_rental----------')
+    # for batch run
+    # dataset_name = "mN2h_1_5b2"
+    # for i in range(3):
+    #     print('----------MTGP_niching_rental_original_' + str(i) + '_----------')
+    #     seed = i
+    #     GPmain_niching_rental_original.main(dataset_name, seed)
+    #
+    # for i in range(3):
+    #     print('----------CCGP_niching_rental_original_' + str(i) + '_----------')
+    #     seed = i
+    #     CCGPmain_niching_rental_original.main(dataset_name, seed)
+    #
+    # dataset_name = "lN2h_1_5b2"
+    # for i in range(3):
+    #     print('----------MTGP_niching_rental_original_' + str(i) + '_----------')
+    #     seed = i
+    #     GPmain_niching_rental_original.main(dataset_name, seed)
+    #
+    # for i in range(3):
+    #     print('----------CCGP_niching_rental_original_' + str(i) + '_----------')
+    #     seed = i
+    #     CCGPmain_niching_rental_original.main(dataset_name, seed)
+    #
+    # for i in range(3):
+    #     print('----------PSO_rental_' + str(i) + '_----------')
+    #     seed = i
     #     PSO_main_rental.main(dataset_name, seed)
-    # elif algo == 'PSO_rental_3':
-    #     for i in range(3):
-    #         print('----------PSO_rental_'+str(i)+'_----------')
-    #         seed = i
-    #         PSO_main_rental.main(dataset_name, seed)
-    # elif algo == 'DRL':
-    #     print('----------DRL----------')
-    #     DRL_main.main(dataset_name, seed)
-    # elif algo == 'testRuleMTGP':
-    #     print('----------testRuleMTGP----------')
-    #     testRuleMTGP.main(dataset_name, seed)
-    # elif algo == 'S2Demo':
-    #     print('----------S2Demo----------')
-    #     S2Demo.main(dataset_name, seed)
-    # elif algo == 'testRuleCCGP':
-    #     print('----------testRuleCCGP----------')
-    #     testRuleCCGP.main(dataset_name, seed)
-    # elif algo == 'testRuleDRL':
-    #     print('----------testRuleDRL----------')
-    #     testRuleDRL.main(dataset_name, seed)
-    # elif algo == 'trainRulesSPolicy':
-    #     print('----------trainAndTestRulesSPolicy----------')
-    #     trainRulesSPolicy.main(dataset_name, seed)
